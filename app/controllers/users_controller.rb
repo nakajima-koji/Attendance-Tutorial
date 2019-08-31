32b93b845e6b32586fbe6698efbbf7ffee894110
koji_nakajima <crazy.max.meteora@gmail.com>
2019-08-31 12:41:08 +0000

    5-5-0終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
+ 
   def show
     @user = User.find(params[:id])
+    #debugger  インスタンス変数を定義した直後にこのメソッドが実行されます。
   end
   
   def new
+    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
   end
 end
