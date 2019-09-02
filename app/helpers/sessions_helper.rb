4f1736c11d80c6d0ecf904a620104b6f9bbb17b7
koji_nakajima <crazy.max.meteora@gmail.com>
2019-09-02 13:26:28 +0000

    6-3が終了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,7 +8,7 @@ module SessionsHelper
   # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
   def current_user
     if session[:user_id]
-      @current_user ||= User.find_by(id: session[:id])
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 end
