afe3cc3d196ffd5d3a7e94458ea1fa8c6abfb0a2
koji_nakajima <crazy.max.meteora@gmail.com>
2019-09-02 13:25:23 +0000

    6-3終了

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン後にユーザー情報ページにリダイレクトします。
+      log_in user
+      redirect_to user
+    else
+      # ここにはエラーメッセージ用のflashを入れます。
+      flash.now[:danger] = '認証に失敗しました。'
+      render :new
+    end
+  end
+end
