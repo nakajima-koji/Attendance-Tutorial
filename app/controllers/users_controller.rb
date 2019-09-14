8ad81cdcdce8737216413b25110fb2ebf8002e67
koji_nakajima <crazy.max.meteora@gmail.com>
2019-09-14 00:15:25 +0000

    Finish editing-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit,:update]
-  before_action :logged_in_user, only: [:show, :edit, :update ]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
- 
+  before_action :admin_user, only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
     #debugger  インスタンス変数を定義した直後にこのメソッドが実行されます。
   end
@@ -34,6 +39,12 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+  
   private
   
   # ログイン済みのユーザーが確認します。
@@ -61,6 +72,11 @@ class UsersController < ApplicationController
     # アクセスしたユーザーが現在ログインしているユーザーが確認します。
     def correct_user
       redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # システム管理権限所有かどうか判定します。
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
 
